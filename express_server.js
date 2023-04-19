525dc1dbc38a4f14e149136908cab8e27e556dc0
Tamara Tell <2telltamara@gmail.com>
2023-04-19 04:34:23 -0400

    first half of project complete

--- express_server.js
+++ express_server.js
@@ -4,19 +4,20 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
-const generateRandomString = () => {
+
+const generateShortURL = () => {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let result = '';
+  let id = '';
   for (const character of characters) {
-    if (result.length !== 6) {
-      result += characters.charAt(Math.floor(Math.random() * characters.length));
+    if (id.length !== 6) {
+      id += characters.charAt(Math.floor(Math.random() * characters.length));
     }
-  } return result;
+  } return id;
 };
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
 };
 
 app.use(express.urlencoded({ extended: true }));
@@ -34,14 +35,22 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
-  res.render("urls_show", templateVars);
-});
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const id = generateShortURL();
+  const longURL = req.body.longURL;
+  urlDatabase[id] = longURL;
+  res.redirect(`/urls/${id}`);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  if (!longURL) {
+    res.status(404).send("Short URL not found");
+  } else {
+    res.redirect(longURL);
+  }
 });
 
 app.listen(PORT, () => {
