c9b55721a3cbaa94bf147c67b30aad54b5bbf129
Tamara Tell <2telltamara@gmail.com>
2023-04-19 00:32:44 -0400

    added urls_show.ejs

--- express_server.js
+++ express_server.js
@@ -13,17 +13,14 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
-
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
-
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: };
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
